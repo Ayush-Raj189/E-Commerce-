d87cf2bf479e8a8e2099263b5d6a8225377d65a6
Ayush-Raj189 <ayushashish1111@gmail.com>
2025-10-25 15:51:39 +0530

    All Errors Fixed

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -16,7 +16,7 @@ const ProductImageUpload = ({ imageFile,
     setUploadedImageUrl,
     setImageLoadingState,
     isEditMode,
-    isCustomStyling=false }) => {
+    isCustomStyling = false }) => {
     const inputRef = useRef(null);
 
     const handleDragOver = (e) => {
@@ -49,7 +49,7 @@ const ProductImageUpload = ({ imageFile,
         const data = new FormData();
         data.append("my_file", imageFile);
         const response = await axios.post(
-            "http://localhost:5000/api/admin/products/upload-image",
+            `${import.meta.env.VITE_API_URL}/api/admin/products/upload-image`,
             data
         );
         console.log(response, "response");
@@ -66,8 +66,8 @@ const ProductImageUpload = ({ imageFile,
 
     return (
         <div
-      className={`w-full  mt-4 ${isCustomStyling ? "" : "max-w-md mx-auto"}`}
-    >
+            className={`w-full  mt-4 ${isCustomStyling ? "" : "max-w-md mx-auto"}`}
+        >
             <Label className='text-lg font-semibold mb-2 block ml-4 h '>Upload Image</Label>
             <div onDragOver={handleDragOver} onDrop={handleDrop} className={` ${isEditMode ? "opacity-50" : ""} m-4 border-2 border-dashed border-gray-300 rounded-lg p-4`}>
                 <Input className='ml-4 hidden' id="image-upload" type="file" ref={inputRef} onChange={handleImageChange} disabled={isEditMode} />
@@ -76,23 +76,23 @@ const ProductImageUpload = ({ imageFile,
                         <Label htmlFor="image-upload" className={`${isEditMode ? "cursor-not-allowed" : ""} flex flex-col items-center justify-center h-32 cursor-pointer`}>
                             <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
                             <span>Drag & drop or click to upload image</span>
-                        </Label>) : ( imageLoadingState ? <Skeleton className='h-10 bg-gray-100'/> :
-                        <div className='flex items-center justify-between'>
-                            <div className='flex items-center'>
-                                <FileIcon className="w-8 text-primary mr-2 h-8" />
-                                <p className="text-sm font-medium">{imageFile.name}</p>
-                            </div>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="text-muted-foreground hover:text-foreground"
-                                onClick={handleRemoveImage}
-                            >
-                                <XIcon className="w-4 h-4" />
-                                <span className="sr-only">Remove File</span>
-                            </Button>
+                        </Label>) : (imageLoadingState ? <Skeleton className='h-10 bg-gray-100' /> :
+                            <div className='flex items-center justify-between'>
+                                <div className='flex items-center'>
+                                    <FileIcon className="w-8 text-primary mr-2 h-8" />
+                                    <p className="text-sm font-medium">{imageFile.name}</p>
+                                </div>
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="text-muted-foreground hover:text-foreground"
+                                    onClick={handleRemoveImage}
+                                >
+                                    <XIcon className="w-4 h-4" />
+                                    <span className="sr-only">Remove File</span>
+                                </Button>
 
-                        </div>
+                            </div>
 
                     )}
             </div>
